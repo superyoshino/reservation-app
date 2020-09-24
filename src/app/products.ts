eea1e12b9bb981518b389a68ae2f81422f84571a
yoshino <h.yoshino2@fujinetworks.co.jp>
2020-09-24 17:41:13 +0900

    商品詳細ページ

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,22 +1,38 @@
 export const products = [
   {
+    coverImage: './assets/img/phone-cover.jpg',
     name: 'IPhone 11',
     price: 999,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    heading1:'heading1',
+    heading2:'heading2',
+    heading3:'heading3'
   },
   {
+    coverImage: './assets/img/phone-cover.jpg',
     name: 'IPhone X',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    heading1:'heading1',
+    heading2:'heading2',
+    heading3:'heading3'
   },
   {
+    coverImage: './assets/img/phone-cover.jpg',
     name: 'IPhone 8',
     price: 499,
-    description: 'IPhone 8 2018'
+    description: 'IPhone 8 2018',
+    heading1:'heading1',
+    heading2:'heading2',
+    heading3:'heading3'
   },
   {
+    coverImage: './assets/img/phone-cover.jpg',
     name: 'IPhone 7',
     price: 299,
-    description: 'IPhone 7 2016'
+    description: 'IPhone 7 2016',
+    heading1:'heading1',
+    heading2:'heading2',
+    heading3:'heading3'
   }
 ];
