ad33218107b6e1c102d39ac41b10cf5e1d7c3a2b
yoshino <h.yoshino2@fujinetworks.co.jp>
2020-09-24 16:00:20 +0900

    フロント側リストページ作成

---
+++ src/app/products.ts
@@ -0,0 +1,22 @@
+export const products = [
+  {
+    name: 'IPhone 11',
+    price: 999,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'IPhone X',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'IPhone 8',
+    price: 499,
+    description: 'IPhone 8 2018'
+  },
+  {
+    name: 'IPhone 7',
+    price: 299,
+    description: 'IPhone 7 2016'
+  }
+];
