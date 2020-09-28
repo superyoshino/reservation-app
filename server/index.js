73ea85a49375d67ac68c19c57243d036e04ac179
yoshino <h.yoshino2@fujinetworks.co.jp>
2020-09-28 17:23:27 +0900

    検証変更

--- server/index.js
+++ server/index.js
@@ -12,9 +12,10 @@ mongoose.connect(config.DB_URI, {
   useCreateIndex: true
 }).then(
   () => {
+    debugger
     if(process.env.NODE_ENV !== 'production'){
       const fakeDB = new FakeDB()
-      //fakeDB.initDB()
+      fakeDB.initDB()
     }
   }
 )
@@ -22,14 +23,14 @@ mongoose.connect(config.DB_URI, {
 const app = express()
 
 app.use('/api/v1/products', productRoutes)
-
-if(process.env.NODE_ENV === 'production'){
+debugger
+//if(process.env.NODE_ENV === 'production'){
   const appPath = path.join(__dirname,'..','dist','reservation-app')
   app.use(express.static(appPath))
   app.get("*",function(req,res){
     res.sendFile(path.resolve(appPath,'index.html'))
   })
-}
+//}
 
 
 const PORT = process.env.PORT || '3001'
